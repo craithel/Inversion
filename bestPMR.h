15764e8cc288ddc946400ea78b740878b8a9d5ac
Carolyn Raithel <craithel@email.arizona.edu>
2016-06-22 14:18:46 -0700

    Header file for bestP_MR mpi and gpu programs

---
+++ bestPMR.h
@@ -0,0 +1,8 @@
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+int kernel_driver(int myid, int n1overE, double *P1_1overE, double *P2_1overE, double *P3_1overE, double *P4_1overE, double *P5_1overE, double *rho_SLY_host, double *p_SLY_host, double *eps_SLY_host, int nSLY_host);
+#ifdef __cplusplus
+}
+#endif
