3ea0bd0921fc08e1b8918839a97e17330da09ce4
Carolyn Raithel <craithel@email.arizona.edu>
2016-08-29 12:15:29 -0700

    Header modified for openMP-based program (replacing gpus)

--- bestPMR.h
+++ bestPMR.h
@@ -2,7 +2,7 @@
 #ifdef __cplusplus
 extern "C"{
 #endif
-int kernel_driver(int myid, int n1overE, double *P1_1overE, double *P2_1overE, double *P3_1overE, double *P4_1overE, double *P5_1overE, double *rho_SLY_host, double *p_SLY_host, double *eps_SLY_host, int nSLY_host);
+int kernel_driver(char *MRfile, int myid, int nPerProc, double *P1_1overE, double *P2_1overE, double *P3_1overE, double *P4_1overE, double *P5_1overE, double *rho_SLY_in, double *p_SLY_in, double *epsilon_SLY_in, int nSLY_in);
 #ifdef __cplusplus
 }
 #endif
